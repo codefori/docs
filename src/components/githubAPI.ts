0fb9fdd6014213a0dbec4801898410215bde10de
Joseph Wright <Joseph@Wright400.com>
2024-02-29 08:05:45 -0800

    Added limiter to fetchFromGitHub function and add link card to release notes

--- src/components/githubAPI.ts
+++ src/components/githubAPI.ts
@@ -7,20 +7,29 @@ const owner = 'codefori';
 const repo = 'vscode-ibmi';
 const token = process.env.GITHUB_TOKEN || '';
 
-async function fetchFromGitHub(endpoint: string) {
-    console.log(token); 
+/**
+ * Fetch data from GitHub API.
+ *
+ * @param {string} endpoint - The API endpoint to fetch from.
+ * @param {number} limit - The maximum number of items to fetch (default is 5).
+ * @return {Promise<any>} The data from the API, or null if an error occurred.
+ */
+async function fetchFromGitHub(endpoint: string, limit: number = 5) {
+ 
     if (!token) {
         console.error('No GitHub token provided. Setup .env file.');
         return null;
     }
 
-    const url = `${baseURL}/${owner}/${repo}/${endpoint}`;
+    const url = new URL(`${baseURL}/${owner}/${repo}/${endpoint}`);
+    url.searchParams.append('per_page', String(limit));
+
     const headers = {
         'Authorization': `token ${token}`,
         'Accept': 'application/vnd.github.v3+json'
     };
     try {
-        const response = await fetch(url, { headers });
+        const response = await fetch(url.toString(), { headers });
         if (!response.ok) {
             console.error(`HTTP error! status: ${response.status}`);
             return null;
