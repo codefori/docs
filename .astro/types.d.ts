a39fa82e2e5111b290e4634c85cef4157d59f820
worksofliam <mrliamallan@live.co.uk>
2024-06-02 09:42:47 -0400

    Workshop page
    Signed-off-by: worksofliam <mrliamallan@live.co.uk>

--- .astro/types.d.ts
+++ .astro/types.d.ts
@@ -388,6 +388,13 @@ declare module 'astro:content' {
   collection: "docs";
   data: InferEntrySchema<"docs">
 } & { render(): Render[".mdx"] };
+"workshop.mdx": {
+	id: "workshop.mdx";
+  slug: "workshop";
+  body: string;
+  collection: "docs";
+  data: InferEntrySchema<"docs">
+} & { render(): Render[".mdx"] };
 };
 
 	};
@@ -398,5 +405,5 @@ declare module 'astro:content' {
 
 	type AnyEntryMap = ContentEntryMap & DataEntryMap;
 
-	export type ContentConfig = typeof import("./../src/content/config.js");
+	export type ContentConfig = typeof import("../src/content/config.js");
 }
