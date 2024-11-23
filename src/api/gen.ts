9a1a5924da45231fe9a1a9f1dc9fba21a85c118e
worksofliam <mrliamallan@live.co.uk>
2024-11-23 12:49:41 -0500

    Initial work on generated docs
    Signed-off-by: worksofliam <mrliamallan@live.co.uk>

---
+++ src/api/gen.ts
@@ -0,0 +1,111 @@
+import { ClassDeclaration, InterfaceDeclaration, TypescriptParser } from "typescript-parser";
+import path from "path";
+import { stat, writeFile } from "fs/promises";
+
+const exists = (path: string): Promise<boolean> => {
+  return new Promise((resolve) => {
+    stat(path).then(() => resolve(true)).catch(() => resolve(false));
+  });
+}
+
+const PRIVATE = 0;
+let parsedFiles: string[] = [];
+const parser = new TypescriptParser();
+
+// or a filepath
+const WORKSPACE = 'node_modules/@halcyontech/vscode-ibmi-types/';
+
+const parseFile = async (tsPath: string) => {
+  if (parsedFiles.includes(tsPath)) {
+    return [];
+  }
+
+  const parsed = await parser.parseFile(tsPath, WORKSPACE);
+
+  parsedFiles.push(tsPath);
+
+  const baseName = path.basename(tsPath);
+  // console.log(tsPath);
+
+  let id = parsed.identifier;
+  let interfaces: string[] = [];
+  let classes: string[] = [];
+
+  for (const variable of parsed.declarations) {
+    if (variable instanceof InterfaceDeclaration) {
+      interfaces.push(`### ${variable.name}`, ``, `#### Properties`, ``);
+      for (const property of variable.properties) {
+        interfaces.push(`- \`${property.name}${property.isOptional ? `?` : ``}: ${property.type}\``);
+      }
+      interfaces.push(``);
+      
+    } else if (variable instanceof ClassDeclaration) {
+      classes.push(`### ${variable.name}`, ``);
+
+      const staticProps = variable.properties.filter(p => p.isStatic && p.visibility !== PRIVATE);
+      const staticMethods = variable.methods.filter(m => m.isStatic && m.visibility !== PRIVATE);
+
+      if (staticProps.length > 0 || staticMethods.length > 0) {
+        classes.push(`#### Static`, ``);
+        for (const prop of staticProps) {
+          classes.push(`- \`${prop.name}: ${prop.type}\``);
+        }
+        for (const prop of staticMethods) {
+          classes.push(`- \`${prop.isAsync ? `async ` : ``}${prop.name}(${prop.parameters.map(p => `${p.name}: ${p.type}`).join(', ')}): ${prop.type}\``);
+        }
+        classes.push(``);
+      }
+
+      const ctor = variable.ctor;
+
+      if (ctor) {
+        classes.push(`#### Constructor`, ``);
+        classes.push(`- \`${variable.name}(${ctor.parameters.map(p => `${p.name}: ${p.type}`).join(', ')}): ${variable.name}\``);
+        classes.push(``);
+      }
+
+      const instanceProps = variable.properties.filter(p => !p.isStatic && p.visibility !== PRIVATE);
+      const instanceMethods = variable.methods.filter(m => !m.isStatic && m.visibility !== PRIVATE);
+
+      if (instanceProps.length > 0) {
+        classes.push(`#### Properties`, ``);
+        for (const prop of instanceProps) {
+          classes.push(`- \`${prop.name}${prop.isOptional ? `?` : ``}: ${prop.type}\``);
+        }
+        classes.push(``);
+      }
+
+      if (instanceMethods.length > 0) {
+        classes.push(`#### Methods`, ``);
+        for (const prop of instanceMethods) {
+          classes.push(`- \`${prop.isAsync ? `async ` : ``}${prop.name}(${prop.parameters.map(p => `${p.name}: ${p.type}`).join(', ')}): ${prop.type}\``);
+        }
+        classes.push(``);
+      }
+
+    } else {
+      console.log(variable);
+    }
+  }
+
+  // console.log(interfaces);
+
+  const imports = parsed.imports;
+  let subLines: string[] = [];
+  for (const importDetail of imports) {
+    if (importDetail.libraryName.startsWith('.')) {
+      const dPath = path.join(parsed.filePath, `..`, importDetail.libraryName + `.d.ts`);
+      if (await exists(dPath)) {
+        subLines.push(...await parseFile(dPath));
+      } else {
+        console.log(`Could not find ${dPath}`);
+      }
+    }
+  }
+
+  return [`## ${baseName}`, ...classes, ...interfaces, ...subLines];
+}
+
+const lines = await parseFile('node_modules/@halcyontech/vscode-ibmi-types/typings.d.ts');
+
+await writeFile('output.md', lines.join('\n'));
